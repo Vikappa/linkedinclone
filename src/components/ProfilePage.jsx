52e5b85d9d732c06b9c71370f1c7653da4c477f9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-19 11:10:41 +0100

    Base comune

---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,20 @@
+import { Col } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
+
+function ProfilePage(){
+
+    return (
+        <>
+        <Container>
+            <Row>
+                {/* lato sinistro */}
+            <Col></Col>
+            {/* sidebar */}
+            <Col></Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
+
+export default ProfilePage
