372199ba0b1cf75aec21e9ec2e32a5080d9e4cc4
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-20 00:33:34 +0100

    Modale destro

--- src/components/PezziDiNavBar/ModaleDestro.jsx
+++ src/components/PezziDiNavBar/ModaleDestro.jsx
@@ -1,6 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
+import  gruppiIcona  from './gruppi.png'
+import  learningIcona  from './learning.png'
+import  marketplaceIcona  from './marketplace.png'
+import  pubblicaIcona  from './pubblica.png'
+import  pubblicizzaIcona  from './pubblicizza.png'
+import  talentIcona  from './talent.png'
+import  vendiIcona  from './vendi.png'
 import './ModaleDestro.css'
 
 const ModaleDestro = (props) => {
@@ -16,24 +24,109 @@ const ModaleDestro = (props) => {
 
     return (
         <>
-
-            <Modal show={show} onHide={handleClose} className="modaleDestro modal fade modal-right" backdrop={false} centered="true" >
-
+        <Modal show={show} onHide={handleClose} className="modaleDestro modal fade modal-right" backdrop={true} centered="true" >
         <Modal.Header closeButton >
-          <Modal.Title id="">
-            Custom Modal Styling
+          <Modal.Title id="titoloModaleDestro">
+           Per le aziende
           </Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          <p>
-            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-            ipsam atque a dolores quisquam quisquam adipisci possimus
-            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-            deleniti rem!
-          </p>
+            
+        <ListGroup>
+        <ListGroup.Item class="">Scopri altri prodotti LinkedIn</ListGroup.Item>
+        <ListGroup.Item className='d-flex flex-wrap justify-content-start '>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={learningIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>learning</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={talentIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Talent Insights</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center position-relative'>
+                    <img src={pubblicaIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Pubblica un</p><p className='sottoTitoloIconeModaleDestro position-absolute'> offerta di lavoro</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={pubblicizzaIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Pubblicizza</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={vendiIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Vendi</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={gruppiIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Gruppi</p>
+                </div>
+            </div>
+            <div className='m-1 mx-2 pulsanteIconaModaleDestro d-flex align-items-center justify-content-center '>
+                <div style={{width:"20%"}} className='d-flex flex-column justify-content-center align-items-center'>
+                    <img src={marketplaceIcona} alt="gruppiIcona" height={"36px"} />
+                    <p style={{fontSize:"0.8rem"}} className='sottoTitoloIconeModaleDestro'>Marketplace dei servizi</p>
+                </div>
+            </div>
+        </ListGroup.Item>
+        </ListGroup>
+
+        <ListGroup className='my-2'>
+        <ListGroup.Item style={{fontSize:"0.9rem"}}>
+            
+            Scopri altro per il tuo Business
+            
+            </ListGroup.Item>
+
+        <ListGroup.Item className='listItemModaleDestro' >
+            <div className='d-flex flex-column p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Assumi su LinkedIn</p>
+                <p style={{fontSize:"0.6rem"}} className='m-0 sottoTitoloModaleDestro' >Trova, attrai e assumi</p>
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='listItemModaleDestro' >
+            <div className='d-flex flex-column p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Vendi con Linkedin</p>
+                <p style={{fontSize:"0.6rem"}} className='m-0 sottoTitoloModaleDestro' >Sblocca nuove opportunità di vendita</p>
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='listItemModaleDestro' >
+            <div className='d-flex flex-column p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Offerta di lavoro gratuita</p>
+                <p style={{fontSize:"0.6rem"}} className='m-0 sottoTitoloModaleDestro' >Ottieni rapidamente candidati qualificati</p>
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='listItemModaleDestro' >
+            <div className='d-flex flex-column p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Impara con LinkedIn</p>
+                <p style={{fontSize:"0.6rem"}} className='m-0 sottoTitoloModaleDestro' >Corsi per formare i tuoi dipendenti</p>
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='listItemModaleDestro' >
+            <div className='d-flex flex-column p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Centro amministrazione</p>
+                <p style={{fontSize:"0.6rem"}} className='m-0 sottoTitoloModaleDestro' >Gestisci i dettagli di fatturazione e account</p>
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='d-flex align-items-center fw-bold listItemModaleDestro'>
+            <div className='d-flex align-items-center gap-2 p-0 m-0'>
+                <p className='p-0 m-0'  style={{fontSize:"0.8rem"}}>Crea pagina aziendale</p>
+                <i className="bi bi-plus-circle"></i>
+            </div>
+        </ListGroup.Item>
+        </ListGroup>
+
+
         </Modal.Body>
       </Modal>
         </>
