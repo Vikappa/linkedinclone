5ecfba4e27b17586beacaf7e0a5f6cdbb1088fee
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-19 19:56:55 +0100

    update

--- src/components/LinkedInNavBar.jsx
+++ src/components/LinkedInNavBar.jsx
@@ -10,7 +10,7 @@ function LinkedInNavBar() {
 
   return (
     <>
-    <Navbar fixed='top' className="bg-body-tertiary justify-content-between container-fluid w-100 p-md-0">
+    <Navbar fixed='top' className="bg-body-tertiary justify-content-between container-fluid w-100 p-sm-0">
         <img src={profilePic} alt="profile pic mockup" className="rounded-circle d-sm-none" id='profilePic' />
       <Container className='p-0 d-sm-none'>
         <Form className='w-100 d-flex align-items-center'>
@@ -26,38 +26,54 @@ function LinkedInNavBar() {
       <Link>
       <i className="fa-solid fa-comment-dots d-sm-none" id="messageIcon"></i>
       </Link>
-        <img src={linkedInIcon} alt='linkedin icon' height={'30px'} className='d-flex align-items-center mx-sm-3' id='linkedInIcon' />
-      <Container className='container-fluid d-none d-sm-flex'>
+        <img src={linkedInIcon} alt='linkedin icon' height={'30px'} className='d-none d-sm-flex d-flex align-items-center mx-sm-3' id='linkedInIcon' />
+      <Container className=' d-none d-sm-flex flex-grow-1 sectionOneNavbar'>
 
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-search navBarDedicatedButton " > </i>
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Cerca</p>
         </div>
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-house-door-fill navBarDedicatedButton " > </i>
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Home</p>
         </div>
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-person-fill navBarDedicatedButton " > </i>
+        <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Rete</p>
+        </div>
+        <div className='d-flex flex-column text-center'>
+        <i className="bi bi-briefcase-fill navBarDedicatedButton " > </i>
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Lavoro</p>
         </div>
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-chat-dots-fill navBarDedicatedButton " > </i>
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Messaggistica</p>
         </div>
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-bell-fill navBarDedicatedButton " > </i>
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Notifiche</p>
         </div>
         <div className='d-flex flex-column text-center'>
-        <i className="bi bi-search"></i>
+        <img src={profilePic} alt="profile pic mockup" className="rounded-circle" style={{height:"20px"}}  />
         <p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Tu</p>
         </div>
-
       </Container>
+
+      <Container className='container-fluid d-none d-sm-flex'>
+
+<div className='d-flex flex-column text-center'>
+<i className="bi bi-x-diamond-fill"></i>
+<p className='d-none d-lg-inline p-0 m-0 navBarDedicatedButtonP'>Cerca</p>
+</div>
+
+<div className='d-flex flex-column text-center goliardiaPortamiVia'>
+  <a className='navBarDedicatedButtonP'>Prova Epicode e paga tra 2 anni!</a>
+  </div>
+
+</Container>
     </Navbar>
 
-<Navbar fixed='bottom' className="bg-body-tertiary justify-content-between container-fluid w-100 d-md-none p-0">
+<Navbar fixed='bottom' className="bg-body-tertiary justify-content-between container-fluid w-100 d-sm-none p-0">
   <Link className='navbar2Buttons' >
 <i className="bi bi-house-door-fill"></i>
   </Link>
