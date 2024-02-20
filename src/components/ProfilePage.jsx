15740bfee321ed72c0d20ab9c25715ba5dcc9f62
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-02-20 10:12:57 +0100

    sidebarIntoMain

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,9 +1,10 @@
-import { Col } from "react-bootstrap"
-import { Container, Row } from "react-bootstrap"
-import LinkedInNavBar from "./LinkedInNavBar"
-import Analisi from "./Analisi"
-import Footer from "./Footer"
-import './style.css'
+import { Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import LinkedInNavBar from "./LinkedInNavBar";
+// import Analisi from "./Analisi";
+import Footer from "./Footer";
+import "./style.css";
+import SideBar from "./SideBar";
 
 function ProfilePage() {
   return (
@@ -12,19 +13,18 @@ function ProfilePage() {
       <Container>
         <Row>
           {/* lato sinistro */}
-          <Col xs={12} lg={9}
-          className="py-3"
-          >
-
+          <Col xs={12} lg={9} className="py-3">
             {/* <Analisi /> */}
-          </Col >
+          </Col>
           {/* sidebar */}
-          <Col xs={0} lg={3}></Col>
+          <Col xs={0} lg={3}>
+            <SideBar />
+          </Col>
         </Row>
-        <Footer/>
+        <Footer />
       </Container>
     </>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
