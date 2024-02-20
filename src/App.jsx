44583ceeba5bd39e4ea90a399e72860fc67d9e58
Fidan Hykolli <fidan-323@live.com>
2024-02-20 10:43:39 +0100

    ProfileHero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import ProfileCards from './components/ProfileHero'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
-
+    <ProfileCards/>
     </>
   )
 }
---
+++ src/components/ProfileHero.jsx
@@ -0,0 +1,159 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function ProfileCard() {
+  const [profile, setProfile] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    bio: '',
+    email: '',
+    title: '',
+    username: ''
+  });
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMjNiMzI0ZjYwNTAwMTkzN2Q0NzkiLCJpYXQiOjE3MDgzMzYwNTEsImV4cCI6MTcwOTU0NTY1MX0.QwQijjjSxZNMPrGebk1FQ-jba4Y2hhdlK43ytPhzCnc';
+
+  useEffect(() => {
+    fetch('https://striveschool-api.herokuapp.com/api/profile/', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setProfile(data[0]); 
+      setFormData({
+        name: data[0].name,
+        surname: data[0].surname,
+        bio: data[0].bio,
+        email: data[0].email,
+        title: data[0].title,
+        username: data[0].username
+      });
+    })
+    .catch(error => {
+      console.error('Si è verificato un errore:', error);
+    });
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('https://striveschool-api.herokuapp.com/api/profile/', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        setIsEditing(false);
+
+
+        const updatedProfile = await response.json();
+        setProfile(updatedProfile);
+      } else {
+        console.error('Errore durante l\'aggiornamento del profilo:', response.status);
+      }
+    } catch (error) {
+      console.error('Si è verificato un errore:', error);
+    }
+  };
+
+  return (
+    <>
+      {profile && (
+        <Card key={profile._id} style={{ width: '50rem', marginTop: '10px', position: 'relative', overflow: 'hidden', backgroundColor: "white" }}>
+          <div
+            style={{
+              width: '100%',
+              height: '150px',
+              backgroundImage: 'url(./src/img/bg-linkedin.png)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              zIndex: 2,
+              position: 'absolute',
+              top: 0,
+              left: 0,
+            }}
+          />
+          <Card.Body style={{ zIndex: 3, position: 'relative', textAlign: 'left', marginLeft: '20px', top: "2.5rem" }}>
+            <Card.Img className='ProfileImg' src={profile.image} alt="Profile Image" width={100} style={{ borderRadius: "50%", border: "4px solid white" }} />
+            <Card.Title style={{ color: 'black', marginBottom: '10px', position: 'relative', zIndex: 4, fontSize: "40px" }}>
+              {profile.name ? profile.name : <span style={{ color: 'red' }}>Name not available</span>}
+              {' '}
+              {profile.surname ? profile.surname : <span style={{ color: 'red' }}>Surname not available</span>}
+              {!isEditing && <button className='Verify' style={{ marginLeft: '10px' }} onClick={() => setIsEditing(true)}>Modifica</button>}
+            </Card.Title>
+            {!isEditing && (
+              <>
+                <Card.Text style={{ fontSize: "22px", color: profile.email ? 'black' : 'red', marginBottom: '5px', position: 'relative', zIndex: 4 }}>
+                  Email: {profile.email ? profile.email : "Not Available"}
+                </Card.Text>
+                <Card.Text style={{ fontSize: "22px", color: profile.username ? 'black' : 'red', marginBottom: '5px', position: 'relative', zIndex: 4 }}>
+                  Username: {profile.username ? profile.username : "Not Available"}
+                </Card.Text>
+                <Card.Text style={{ fontSize: "22px", color: profile.title ? 'black' : 'red', marginBottom: '5px', position: 'relative', zIndex: 4 }}>
+                  Title: {profile.title ? profile.title : "Not Available"}
+                </Card.Text>
+                <Card.Text style={{ fontSize: "22px", color: profile.bio ? 'black' : 'red', position: 'relative', zIndex: 4 }}>
+                  Bio: {profile.bio ? profile.bio : "Not Available"}
+                </Card.Text>
+              </>
+            )}
+            {isEditing && (
+              <Card style={{ textAlign:"center", marginTop: '20px', padding: '20px', backgroundColor: '#f8f9fa' }}>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="name">
+                    <Form.Label style={{color:"black"}}>Name:</Form.Label>
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Enter your name" />
+                  </Form.Group>
+                  <Form.Group controlId="surname">
+                    <Form.Label style={{color:"black"}}>Surname:</Form.Label>
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} type="text" name="surname" value={formData.surname} onChange={handleInputChange} placeholder="Enter your surname" />
+                  </Form.Group>
+                  <Form.Group controlId="email">
+                    <Form.Label style={{color:"black"}}>Email:</Form.Label>
+
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Enter your email"/>
+                  </Form.Group>
+                  <Form.Group controlId="username">
+                    <Form.Label style={{color:"black"}}>Username:</Form.Label>
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} type="text" name="username" value={formData.username} onChange={handleInputChange} placeholder="Enter your username" />
+                  </Form.Group>
+                  <Form.Group controlId="title">
+                    <Form.Label style={{color:"black"}}>Title:</Form.Label>
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} type="text" name="title" value={formData.title} onChange={handleInputChange} placeholder="Enter your title" />
+                  </Form.Group>
+                  <Form.Group controlId="bio">
+                    <Form.Label style={{color:"black"}}>Bio:</Form.Label>
+                    <Form.Control style={{color:"black", backgroundColor:"white"}} as="textarea" rows={3} name="bio" value={formData.bio} onChange={handleInputChange} placeholder="Enter your bio" />
+                  </Form.Group>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <Button style={{paddingTop:"4px",paddingBottom:"4px",paddingLeft:"15px",paddingRight:"15px",marginLeft:"5px", fontSize:"12px"}} variant="secondary" onClick={() => setIsEditing(false)}>Cancel</Button>
+                    <Button style={{paddingTop:"4px",paddingBottom:"4px",paddingLeft:"15px",paddingRight:"15px",marginLeft:"5px", fontSize:"12px"}} variant="primary" type="submit">Save Changes</Button>
+                  </div>
+                </Form>
+              </Card>
+            )}
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  );
+}
+
+export default ProfileCard;
