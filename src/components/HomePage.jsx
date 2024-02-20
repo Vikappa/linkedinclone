502073363d5b749f47ab6a4740ab071aeaef4188
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-02-20 16:21:02 +0100

    added ProfileBar on main

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,14 @@
-import { useDispatch } from'react-redux'; 
-import { useEffect } from "react" 
-import { FETCH_CURRENT_USER, FETCH_ALL_USERS, ADD_EXPERIENCE } from '../Redux/Actions/ADD_EXPERIENCE';
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import {
+  FETCH_CURRENT_USER,
+  FETCH_ALL_USERS,
+  ADD_EXPERIENCE,
+} from "../Redux/Actions/ADD_EXPERIENCE";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ProfileBarOnHomepage from "./ProfileBarOnHomepage";
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
@@ -14,78 +22,83 @@ const calculateExperience = (startDate, endDate) => {
 };
 
 function HomePage() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("accessToken");
 
-  
-    useEffect(() => {
-  
-  const accessToken = sessionStorage.getItem('accessToken');
-  
-      const fetchCurrentUser = async () => {
-        try {
-          const res = await fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
+    const fetchCurrentUser = async () => {
+      try {
+        const res = await fetch(
+          "https://striveschool-api.herokuapp.com/api/profile/me",
+          {
             headers: {
-              'Authorization': `Bearer ${accessToken?accessToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMjJhODI0ZjYwNTAwMTkzN2Q0NzgiLCJpYXQiOjE3MDgzMzU3ODQsImV4cCI6MTcwOTU0NTM4NH0.pioeDwZO2GA-_tAisq4KElbrIk9InfeCBaG2-L3oQJA"}`
-            }
-          })
-          if (!res.ok) {
-            throw new Error("Errore");
-          }
-          const data = await res.json();
-          console.log(data)
-          dispatch({
-            type: FETCH_CURRENT_USER,
-            payload: data
-          })
-  
-  
-  
-  
-        } catch (error) {
-          console.log("Errore", error);
-        }
-      }
-  
-  
-    
-      const myUrlFetchProfile =
-        "https://striveschool-api.herokuapp.com/api/profile/65d322a824f605001937d478/experiences";
-      const bearerToken =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMjJhODI0ZjYwNTAwMTkzN2Q0NzgiLCJpYXQiOjE3MDgzMzU3ODQsImV4cCI6MTcwOTU0NTM4NH0.pioeDwZO2GA-_tAisq4KElbrIk9InfeCBaG2-L3oQJA";
-    
-      const fetchExperience = async () => {
-        try {
-          const res = await fetch(myUrlFetchProfile, {
-            method: "GET",
-            headers: {
-              Authorization: "Bearer " + bearerToken,
-              "Content-type": "application/json",
+              Authorization: `Bearer ${
+                accessToken
+                  ? accessToken
+                  : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMjJhODI0ZjYwNTAwMTkzN2Q0NzgiLCJpYXQiOjE3MDgzMzU3ODQsImV4cCI6MTcwOTU0NTM4NH0.pioeDwZO2GA-_tAisq4KElbrIk9InfeCBaG2-L3oQJA"
+              }`,
             },
-          });
-    
-          if (!res.ok) {
-            throw new Error("Errore");
           }
-    
-          const data = await res.json();
-          dispatch({type: ADD_EXPERIENCE, payload: data});
-          console.log("experience", data);
-        } catch (error) {
-          console.log("Errore", error);
+        );
+        if (!res.ok) {
+          throw new Error("Errore");
         }
-      };
-  
-  fetchExperience()
-  fetchCurrentUser()
-  
-      //fetch2
-      //fetch3
-    }, [])
+        const data = await res.json();
+        console.log(data);
+        dispatch({
+          type: FETCH_CURRENT_USER,
+          payload: data,
+        });
+      } catch (error) {
+        console.log("Errore", error);
+      }
+    };
 
-return (
-    <></>
-)
+    const myUrlFetchProfile =
+      "https://striveschool-api.herokuapp.com/api/profile/65d322a824f605001937d478/experiences";
+    const bearerToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMjJhODI0ZjYwNTAwMTkzN2Q0NzgiLCJpYXQiOjE3MDgzMzU3ODQsImV4cCI6MTcwOTU0NTM4NH0.pioeDwZO2GA-_tAisq4KElbrIk9InfeCBaG2-L3oQJA";
+
+    const fetchExperience = async () => {
+      try {
+        const res = await fetch(myUrlFetchProfile, {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + bearerToken,
+            "Content-type": "application/json",
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Errore");
+        }
+
+        const data = await res.json();
+        dispatch({ type: ADD_EXPERIENCE, payload: data });
+        console.log("experience", data);
+      } catch (error) {
+        console.log("Errore", error);
+      }
+    };
+
+    fetchExperience();
+    fetchCurrentUser();
+
+    //fetch2
+    //fetch3
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={3}>
+          <ProfileBarOnHomepage />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/components/ProfileBarOnHomepage.jsx
@@ -0,0 +1,52 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Image from "react-bootstrap/Image";
+
+const ProfileBarOnHomepage = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <Card className="my-2">
+            <Card.Img
+              variant="top"
+              src="https://img.freepik.com/free-photo/vibrant-colors-blend-abstract-backdrop-pattern-generated-by-ai_188544-9565.jpg"
+            />
+            <Card.Body className="position-relative text-center">
+              <div className="position-absolute top-0 start-50 translate-middle">
+                <Image
+                  src="https://placebear.com/g/200/300"
+                  roundedCircle
+                  fluid
+                  thumbnail
+                  style={{ width: "80px", height: "80px" }}
+                  className="border-1"
+                />
+              </div>
+              <Card.Title className="mt-5">Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush d-none d-md-block">
+              <ListGroup.Item>
+                <p className="text-secondary m-0">Collegamenti</p>
+                <p>Espandi la tua rete</p>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Raggiungi i tuoi obiettivi con Premium
+                <span>Prova Premium per 0 EUR</span>
+              </ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default ProfileBarOnHomepage;
