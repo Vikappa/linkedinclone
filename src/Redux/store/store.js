ee143f72c28c920b975bd6f7795ea6a84d34ad38
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-21 02:17:39 +0100

    Update slice store

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -4,12 +4,14 @@ import experienceReducer from "../Reducers/experienceReducer";
 import currentUserReducer from "../Reducers/fetchCurrentUser";
 import allUserReducer from '../Reducers/fetchAllUsers'
 import fetchAllPostReducer from "../Reducers/fetchAllPostReducer";
+import reducerInspectedUser from "../Reducers/reducerInspectedUser";
 
 const mainReducer = combineReducers({
   experience: experienceReducer,
   currentUser: currentUserReducer,
   allUserArray: allUserReducer,
-  arrayPosts: fetchAllPostReducer
+  arrayPosts: fetchAllPostReducer,
+  inspectedUser: reducerInspectedUser
 })
 
 const store = configureStore({
