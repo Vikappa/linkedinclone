aa519474b073997f1fa09d43c16b6b737daa0cb0
Alexloreto125 <alexloreto125@gmail.com>
2024-02-21 13:49:40 +0100

    POST SENZA GET

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,73 +1,139 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import React from 'react'
+import React from "react";
 import ModaleAggiungiEsperienza from "./ModaleAggiungiEsperienza";
-
+import { useDispatch } from "react-redux";
+import { FETCH_CURRENT_USER_EXPERIENCES } from "../Redux/Actions/ADD_EXPERIENCE";
 
 function formatISODate(isoString) {
   const date = new Date(isoString);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
-  return `${day} ${month} ${year}`; 
+  return `${day} ${month} ${year}`;
 }
 
 const Experience = () => {
+  let currentUserStore = useSelector((state) => state.currentUser.currentUser);
+  const [currentUser, setCurrentUser] = useState(currentUserStore);
 
-  const storeEsperienze = useSelector(state => state.experience.experiences)
-  const [esperienze, setEsperienze] = useState(storeEsperienze)
-  const [visibilitàModaleAddEsperienza, setVisibilitàModaleAddEsperienza] = useState(false)
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    setCurrentUser(currentUserStore);
+  }, [currentUserStore]);
+
+  const storeEsperienze = useSelector((state) => state.experience.experiences);
+  const [esperienze, setEsperienze] = useState(storeEsperienze);
+  const [visibilitàModaleAddEsperienza, setVisibilitàModaleAddEsperienza] =
+    useState(false);
+
+  useEffect(() => {
+    setEsperienze(esperienze);
+  }, [storeEsperienze]);
   const handleAddEsperienza = (e) => {
     e.preventDefault();
     console.log("show");
     setVisibilitàModaleAddEsperienza(true);
   };
-  
+
   const handleChiudiAddEsperienza = () => {
+    const fetchExperiencesCurrentUser = async () => {
+      try {
+        const fetchedExperiences = await fetch(
+          `https://striveschool-api.herokuapp.com/api/profile/${currentUser._id}/experiences`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            },
+          }
+        );
+
+        if (!fetchedExperiences.ok) {
+          throw new Error("Errore nella fetch delle esperienze");
+        }
+
+        const experiencesData = await fetchedExperiences.json();
+
+        // Dispatch dell'azione Redux
+        await dispatch({
+          type: FETCH_CURRENT_USER_EXPERIENCES,
+          payload: experiencesData,
+        });
+      } catch (error) {
+        console.log("Errore", error);
+      }
+    };
+    fetchExperiencesCurrentUser();
+
     setVisibilitàModaleAddEsperienza(false);
   };
-  
-
 
   return (
     <>
-<div style={{border:"1px solid #D2D2D2"}} className="container border-1 rounded-3 d-flex flex-column bg-white my-2 p-3">
-  <div className="d-flex align-items-center justify-content-between">
-    <h5 className="p-1">Esperienza</h5>
-    <i className="bi bi-plus-circle" style={{fontSize:"2rem"}}
-    onClick={handleAddEsperienza}
-    ></i>
-  </div>
-  {esperienze.map((esperienza,index) => (
-    <div key={index} className="d-flex align-items-center ">
-<div className="d-flex justify-content-start align-items-center jobLine p-2">
-<i className="bi bi-briefcase-fill p-2" style={{fontSize:"2.5rem"}}></i>
-<div className="d-flex flex-column align-items-start">
-
-      <p className="m-0">{esperienza.role}</p>
-      <p className="m-0" style={{fontSize:"0.75rem", color:"grey" }} >{esperienza.company}</p>
-      <p className="m-0" style={{fontSize:"0.6rem", color:"grey" }} >{esperienza.description} - {esperienza.area}</p>
-      {esperienza.endDate? 
-            <p className="m-0" style={{fontSize:"0.6rem", color:"grey" }} >{formatISODate(esperienza.startDate)} - {formatISODate(esperienza.endDate)}</p>
-            :
-<p className="m-0" style={{fontSize:"0.6rem", color:"grey" }} >{formatISODate(esperienza.startDate)} - attualmente assunto</p>
-
-}
-</div>
-</div>
-    </div>
-  ))}
-
-</div>
-<ModaleAggiungiEsperienza 
-  visibilitàModaleAddEsperienza={visibilitàModaleAddEsperienza} 
-  setVisibilitàModaleAddEsperienza={setVisibilitàModaleAddEsperienza} 
-  onClose={handleChiudiAddEsperienza} 
-/>
-  </>
-  )
-
-}
+      <div
+        style={{ border: "1px solid #D2D2D2" }}
+        className="container border-1 rounded-3 d-flex flex-column bg-white my-2 p-3"
+      >
+        <div className="d-flex align-items-center justify-content-between">
+          <h5 className="p-1">Esperienza</h5>
+          <i
+            className="bi bi-plus-circle"
+            style={{ fontSize: "2rem" }}
+            onClick={handleAddEsperienza}
+          ></i>
+        </div>
+        {esperienze.map((esperienza, index) => (
+          <div key={index} className="d-flex align-items-center ">
+            <div className="d-flex justify-content-start align-items-center jobLine p-2">
+              <i
+                className="bi bi-briefcase-fill p-2"
+                style={{ fontSize: "2.5rem" }}
+              ></i>
+              <div className="d-flex flex-column align-items-start">
+                <p className="m-0">{esperienza.role}</p>
+                <p
+                  className="m-0"
+                  style={{ fontSize: "0.75rem", color: "grey" }}
+                >
+                  {esperienza.company}
+                </p>
+                <p
+                  className="m-0"
+                  style={{ fontSize: "0.6rem", color: "grey" }}
+                >
+                  {esperienza.description} - {esperienza.area}
+                </p>
+                {esperienza.endDate ? (
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.6rem", color: "grey" }}
+                  >
+                    {formatISODate(esperienza.startDate)} -{" "}
+                    {formatISODate(esperienza.endDate)}
+                  </p>
+                ) : (
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.6rem", color: "grey" }}
+                  >
+                    {formatISODate(esperienza.startDate)} - attualmente assunto
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <ModaleAggiungiEsperienza
+        visibilitàModaleAddEsperienza={visibilitàModaleAddEsperienza}
+        setVisibilitàModaleAddEsperienza={setVisibilitàModaleAddEsperienza}
+        onClose={handleChiudiAddEsperienza}
+      />
+    </>
+  );
+};
 
 export default Experience;
--- src/components/ModaleAggiungiEsperienza.jsx
+++ src/components/ModaleAggiungiEsperienza.jsx
@@ -1,145 +1,209 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  ADD_EXPERIENCE,
+  FETCH_CURRENT_USER_EXPERIENCES,
+} from "../Redux/Actions/ADD_EXPERIENCE";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function ModaleAggiungiEsperienza(props) {
-  const [visibilitàModaleAddEsperienza, setVisibilitàModaleAddEsperienza] = useState(props.visibilitàModaleAddEsperienza)
-  const [newRuolo, setNewRuolo] = useState('');
-  const [newAzienda, setNewAzienda] = useState('');
-  const [newArea, setNewArea] = useState('');
-  const [newDescrizione, setNewDescrizione] = useState('')
+  const [visibilitàModaleAddEsperienza, setVisibilitàModaleAddEsperienza] =
+    useState(props.visibilitàModaleAddEsperienza);
+  const [newRuolo, setNewRuolo] = useState("");
+  const [newAzienda, setNewAzienda] = useState("");
+  const [newArea, setNewArea] = useState("");
+  const [newDescrizione, setNewDescrizione] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  
-  const handleClose = () => {props.onClose()}
+
+  const handleClose = () => {
+    props.onClose();
+  };
   const handleShow = () => props.setVisibilitàModaleAddEsperienza(true);
 
   const handleRuoloChange = (event) => {
-    setNewRuolo(event.target.value)
-  }
+    setNewRuolo(event.target.value);
+  };
 
   const handleAziendaChange = (event) => {
-    setNewAzienda(event.target.value)
-  }
+    setNewAzienda(event.target.value);
+  };
 
   const handleAreaChange = (event) => {
-    setNewArea(event.target.value)
-  }
+    setNewArea(event.target.value);
+  };
 
   const handleDescrizioneChange = (event) => {
-    setNewDescrizione(event.target.value)
+    setNewDescrizione(event.target.value);
+  };
+
+  function handleDateChange(selectedDate, setFunction) {
+    const formattedDate = formatDate(selectedDate);
+    console.log(formattedDate);
+    setFunction(selectedDate);
   }
 
-  function handleDateChange(event) {
-    const formattedDate = formatDate(event.target.value);
-    console.log(formattedDate)
-  }
-  
   function formatDate(date) {
     const d = new Date(date),
-          day = '' + d.getDate(),
-          month = '' + (d.getMonth() + 1),
-          year = d.getFullYear();
-  
-    return [day.padStart(2, '0'), month.padStart(2, '0'), year].join(' ');
+      day = "" + d.getDate(),
+      month = "" + (d.getMonth() + 1),
+      year = d.getFullYear();
+
+    return [year, month.padStart(2, "0"), day.padStart(2, "0")].join(" ");
   }
+  //!
 
+  let currentUserStore = useSelector((state) => state.currentUser.currentUser);
+  const [currentUser, setCurrentUser] = useState(currentUserStore);
 
+  useEffect(() => {
+    setCurrentUser(currentUserStore);
+  }, [currentUserStore]);
+  // console.log("id", currentUser._id);
+  const myPostUrl = ` https://striveschool-api.herokuapp.com/api/profile/${currentUser._id}/experiences`;
 
-useEffect(() => {
-    setVisibilitàModaleAddEsperienza(props.visibilitàModaleAddEsperienza)
-}, [props.visibilitàModaleAddEsperienza])
+  const bearerToken = sessionStorage.getItem("accessToken");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Submit form");
+    try {
+      const formData = {
+        role: newRuolo,
+        company: newAzienda,
+        startDate: formatDate(startDate),
+        endDate: formatDate(endDate),
+        area: newArea,
+        description: newDescrizione,
+      };
+
+      const response = await fetch(myPostUrl, {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + bearerToken,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      // console.log("API response:", response);
+
+      if (!response.ok) {
+        throw new Error("Errore nella richiesta POST");
+      }
+
+      setNewRuolo("");
+      setNewAzienda("");
+      setNewArea("");
+      setNewDescrizione("");
+      setStartDate(new Date());
+      setEndDate(new Date());
+      handleClose();
+    } catch (error) {
+      console.error("Errore durante la richiesta POST:", error);
+    }
+  };
+
+  //!
+  useEffect(() => {
+    // console.log("Effetto collaterale eseguito");
+    setVisibilitàModaleAddEsperienza(props.visibilitàModaleAddEsperienza);
+  }, [props.visibilitàModaleAddEsperienza]);
 
   return (
-    <>
-      <Modal show={visibilitàModaleAddEsperienza} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Aggiungi nuova esperienza</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="idValueRuolo">
-              <Form.Label>Ruolo</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ruolo.."
-                autoFocus
-                value={newRuolo} 
-                onChange={handleRuoloChange}
+    <Modal
+      show={visibilitàModaleAddEsperienza}
+      onHide={handleClose}
+      onSubmit={handleSubmit}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Aggiungi nuova esperienza</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="idValueRuolo">
+            <Form.Label>Ruolo</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ruolo.."
+              autoFocus
+              value={newRuolo}
+              onChange={handleRuoloChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="idValueAzienda">
+            <Form.Label>Azienda</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Azienda.."
+              value={newAzienda}
+              onChange={handleAziendaChange}
+              required
+            />
+          </Form.Group>
+          <div className="d-flex">
+            <Form.Group className="mb-3" controlId="formStartDate">
+              <Form.Label>Data Inizio</Form.Label>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => handleDateChange(date, setStartDate)}
+                dateFormat="dd MM yyyy"
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="idValueAzienda">
-              <Form.Label>Azienda</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Azienda.."
-                value={newAzienda}
-                onChange={handleAreaChange}
-                required
+
+            <Form.Group className="mb-3" controlId="formEndDate">
+              <Form.Label>Data Fine</Form.Label>
+              <DatePicker
+                selected={endDate}
+                onChange={(date) => handleDateChange(date, setEndDate)}
+                dateFormat="dd MM yyyy"
               />
             </Form.Group>
-<div className='d-flex'>
-<Form.Group className="mb-3" controlId="formStartDate">
-        <Form.Label>Data Inizio</Form.Label>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => handleDateChange(date, setStartDate)}
-          dateFormat="dd MM yyyy"
-          required
-        />
-      </Form.Group>
+          </div>
+          <Form.Group className="mb-3" controlId="valueTextAreaDescrizione">
+            <Form.Label>Descrizione</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={newDescrizione}
+              onChange={handleDescrizioneChange} // Aggiorna lo state ogni volta che il contenuto cambia
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formEndDate">
-        <Form.Label>Data Fine</Form.Label>
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => handleDateChange(date, setEndDate)}
-          dateFormat="dd MM yyyy"
-        />
-      </Form.Group>
-</div>
-<Form.Group
-        className="mb-3"
-        controlId="valueTextAreaDescrizione"
-      >
-        <Form.Label>Descrizione</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          value={newDescrizione}
-          onChange={handleDescrizioneChange} // Aggiorna lo state ogni volta che il contenuto cambia
-        />
-      </Form.Group>
-
-            <Form.Group className="mb-3" controlId="idValueArea">
-              <Form.Label>Area</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Area.."
-                value={newArea}
-                onChange={handleAreaChange}
-              />
-            </Form.Group>
-
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-
-        <Button className='d-flex align-items-center justify-content-center' variant="primary" onClick={handleClose}>
-           Invia
-          </Button>
-
-          <Button className='d-flex align-items-center justify-content-center' variant="secondary" onClick={handleClose}>
-            Annulla
-          </Button>
-
-        </Modal.Footer>
-      </Modal>
-    </>
+          <Form.Group className="mb-3" controlId="idValueArea">
+            <Form.Label>Area</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Area.."
+              value={newArea}
+              onChange={handleAreaChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          className="d-flex align-items-center justify-content-center"
+          variant="success"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Invia
+        </Button>
+        <Button
+          className="d-flex align-items-center justify-content-center"
+          variant="secondary"
+          onClick={handleClose}
+        >
+          Annulla
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
