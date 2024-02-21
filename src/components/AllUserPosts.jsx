dd752171003a8a119e3d9facc73921e4a1d164f6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-21 16:21:41 +0100

    Update render Posts

--- src/components/AllUserPosts.jsx
+++ src/components/AllUserPosts.jsx
@@ -21,17 +21,31 @@ function AllUserPosts(){
     return(
         <div>
             {interoStore.slice(-25).map((post, index) => (
-                <div key={index} style={{border:"1px solid lightgrey"}} className="d-flex flex-column bg-white rounded rounded-2 m-1 my-2 p-2">
-<div className="d-flex align-items-center">
-<img src={post.user.image} style={{}} className="m-2" height={"40px"} alt="Post Author Img"/>
-<div className="d-flex align-items-baseline gap-1">                    
-                    <h6>{post.user.name}</h6>
-                    <p style={{fontSize:"0.65rem", color:"grey"}} className="p-0">    alle   {formatDate(post.createdAt)}</p>
-                    </div>
-</div>
+            <div key={index} style={{border:"1px solid lightgrey"}} className="d-flex flex-column bg-white rounded rounded-2 m-1 my-2 p-2 sizePostDiv">
+             
+                <div className="d-flex align-items-center">
 
-                    <p>{post.text}</p>
-                </div> 
+                <img src={post.user.image} style={{}} className="m-2" height={"40px"} alt="Post Author Img"/>
+
+                  <div className="d-flex flex-column justify-content-start">
+
+                  <div className="d-flex align-items-baseline gap-1">                    
+                    <h6 className="m-0">{post.user.name}</h6>
+                    <p style={{fontSize:"0.65rem", color:"grey"}} className=" m-0 p-0">    alle   {formatDate(post.createdAt)}</p>
+                  </div>
+
+                  <div className="d-flex gap-1 align-item-center ">
+                    <p style={{fontSize:"0.7rem", color:"grey"}}>{post.user.title} |</p>
+                    <p style={{fontSize:"0.7rem", color:"grey"}}> {post.user.area}</p>
+                  </div>
+
+                  </div>
+
+
+                </div>
+
+                    <p className="p-2 px-4">{post.text}</p>
+            </div> 
             ))}
         </div>
     )
