0b15b3093f2de60b9321a2851ea315d00e5f73ce
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-22 13:55:13 +0100

    Update Delete post

--- src/components/CommentPostLi.jsx
+++ src/components/CommentPostLi.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
+import { FETCH_ALL_POST_COMMENTS } from "../Redux/Actions/ADD_EXPERIENCE"
+import { useDispatch } from "react-redux"
 
 
 
 function CommentPostLi(props){
-    const [formattedDate, setFormattedDate] = useState(formatDate(props.comment.createdAt));
+const dispatch = useDispatch()
+
+    const [formattedDate, setFormattedDate] = useState(formatDate(props.comment.createdAt))
 
     useEffect(() => {
-        // Questo effetto viene eseguito ogni volta che `createdAt` o `updatedAt` cambiano.
-        // Assicurati che la logica qui gestisca correttamente il caso in cui `updatedAt` potrebbe non essere definito.
-        const dateToFormat = props.comment.updatedAt ? props.comment.updatedAt : props.comment.createdAt;
-        setFormattedDate(formatDate(dateToFormat));
-      }, [props.comment.createdAt, props.comment.updatedAt]); // Dipendenze dell'effetto
+        const dateToFormat = props.comment.updatedAt ? props.comment.updatedAt : props.comment.createdAt
+        setFormattedDate(formatDate(dateToFormat))
+      }, [props.comment.createdAt, props.comment.updatedAt])
     
 
-    const[commentObg, setCommentObg] = useState(props.comment) 
     
     function formatDate(date) {
         if (!date || isNaN(Date.parse(date))) {
-            // Se la data non è valida, restituisce un placeholder o una stringa vuota
-            return "Data non disponibile";
+            return "Carico data.."
         }
     
         const d = new Date(date),
@@ -27,11 +27,60 @@ function CommentPostLi(props){
             month = "" + (d.getMonth() + 1),
             year = d.getFullYear();
     
-        return [year, month.padStart(2, "0"), day.padStart(2, "0")].join("-");
+        return [year, month.padStart(2, "0"), day.padStart(2, "0")].join("-")
+    }
+
+    function handleDeleteComment(e){
+        e.preventDefault();
+    
+        const deleteComment = async function(){
+            try {
+                const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/` + props.comment._id, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: "Bearer " + sessionStorage.getItem("accessToken")
+                    },
+                })
+    
+                if (!response.ok) {
+                    throw new Error('FALLITA FETCH DELETE');
+                }
+                console.log('COMMENTO CANCELLATO')
+                const fetchAllPostComments = async function(){
+                    try{
+                      const result = await fetch(
+                        `https://striveschool-api.herokuapp.com/api/comments/`,
+                        {
+                            headers: {
+                                'Content-Type': 'application/json',
+                                Authorization: "Bearer " + sessionStorage.getItem("accessToken")
+                            },
+                        }
+                      )
+                      if(!result.ok){
+                        throw new Error("Errore GET COMMENTI")
+                      }
+                      const resultFetchCommentiPost = await result.json()
+                      dispatch({
+                        type: FETCH_ALL_POST_COMMENTS,
+                        payload: resultFetchCommentiPost,
+                      })
+                    } catch (error) {
+                      console.log(error)
+                    }
+                  }
+                  fetchAllPostComments()
+    
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    
+        deleteComment()
     }
     
 
-      
     return (
         <>
           {props.comment ? (
@@ -47,7 +96,9 @@ function CommentPostLi(props){
               </span>
     
               <i style={{ color: "grey", fontSize: "0.8rem" }} className="optionCommentIcon ms-auto bi bi-pencil me-1"></i>
-              <i style={{ color: "grey", fontSize: "0.8rem" }} className="optionCommentIcon bi bi-trash3 mx-1"></i>
+              <i
+              onClick={handleDeleteComment}
+              style={{ color: "grey", fontSize: "0.8rem" }} className="optionCommentIcon bi bi-trash3 mx-1"></i>
             </li>
           ) : (
             ""
