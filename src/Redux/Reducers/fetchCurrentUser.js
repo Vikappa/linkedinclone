ea5ce6be2dc9a793321a6e111d412800327ecb19
Alexloreto125 <alexloreto125@gmail.com>
2024-02-22 11:13:03 +0100

    LOG OUT

--- src/Redux/Reducers/fetchCurrentUser.js
+++ src/Redux/Reducers/fetchCurrentUser.js
@@ -1,20 +1,24 @@
-import { FETCH_CURRENT_USER } from '../Actions/ADD_EXPERIENCE';
+import { FETCH_CURRENT_USER } from "../Actions/ADD_EXPERIENCE";
+import { DELETE_CURRENT_USER } from "../Actions/ADD_EXPERIENCE";
 
 const initialState = {
-    currentUser: null
-}
-
+  currentUser: null,
+};
 
 const currentUserReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: action.payload
-            }
-      default:
-        return state
-    }
+  switch (action.type) {
+    case FETCH_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    case DELETE_CURRENT_USER:
+      return {
+        currentUser: null,
+      };
+    default:
+      return state;
   }
+};
 
-export default currentUserReducer
+export default currentUserReducer;
--- src/components/AllUserPosts.jsx
+++ src/components/AllUserPosts.jsx
@@ -1,52 +1,76 @@
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import AddPost from './AddPost.jsx'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import AddPost from "./AddPost.jsx";
 
 function formatDate(date) {
-    const d = new Date(date),
-      day = "" + d.getDate(),
-      month = "" + (d.getMonth() + 1),
-      year = d.getFullYear();
+  const d = new Date(date),
+    day = "" + d.getDate(),
+    month = "" + (d.getMonth() + 1),
+    year = d.getFullYear();
 
-    return [year, month.padStart(2, "0"), day.padStart(2, "0")].join(" ");
-  }
-
-function AllUserPosts(){
-
-    const interoStore = useSelector(state => state.arrayAllPosts.arrayPosts)
-    const currentUser = useSelector(state => state.currentUser.currentUser)
-
-    useEffect(() => {
-        console.log(interoStore.slice(-25))
-    }, [])
-
-    return(
-        <div>
-            {interoStore ? (
-                <>
-                    <AddPost/>
-                    {interoStore.slice(-25).reverse().map((post, index) => (
-                        <div key={index} style={{border:"1px solid lightgrey"}} className="d-flex flex-column bg-white rounded rounded-2 m-1 my-2 p-2 sizePostDiv">
-                            <div className="d-flex align-items-center">
-                                <img src={post.user.image} className="m-2" height={"40px"} alt="Post Author Img"/>
-                                <div className="d-flex flex-column justify-content-start">
-                                    <div className="d-flex align-items-baseline gap-1">                    
-                                        <h6 className="m-0">{post.user.name}</h6>
-                                        <p style={{fontSize:"0.65rem", color:"grey"}} className=" m-0 p-0">alle {formatDate(post.createdAt)}</p>
-                                    </div>
-                                    <div className="d-flex gap-1 align-item-center ">
-                                        <p style={{fontSize:"0.7rem", color:"grey"}}>{post.user.title} |</p>
-                                        <p style={{fontSize:"0.7rem", color:"grey"}}> {post.user.area}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <p className="p-2 px-4">{post.text}</p>
-                        </div>
-                    ))}
-                </>
-            ) : "Caricamento"}
-        </div>
-    )
+  return [year, month.padStart(2, "0"), day.padStart(2, "0")].join(" ");
 }
 
-export default AllUserPosts
+function AllUserPosts() {
+  const interoStore = useSelector((state) => state.arrayAllPosts.arrayPosts);
+  const currentUser = useSelector((state) => state.currentUser.currentUser);
+
+  useEffect(() => {
+    console.log(interoStore.slice(-25));
+  }, []);
+
+  return (
+    <div>
+      {interoStore ? (
+        <>
+          <AddPost />
+          {interoStore
+            .slice(-25)
+            .reverse()
+            .map((post, index) => (
+              <div
+                key={index}
+                style={{ border: "1px solid lightgrey" }}
+                className="d-flex flex-column bg-white rounded rounded-2 m-1 my-2 p-2 sizePostDiv"
+              >
+                <div className="d-flex align-items-center">
+                  <img
+                    src={post.user.image}
+                    className="m-2 border rounded-circle"
+                    height={"48px"}
+                    width={"48px"}
+                    alt="Post Author Img"
+                  />
+                  <div className="d-flex flex-column justify-content-start">
+                    <div className="d-flex align-items-baseline gap-1">
+                      <h6 className="m-0">{post.user.name}</h6>
+                      <p
+                        style={{ fontSize: "0.65rem", color: "grey" }}
+                        className=" m-0 p-0"
+                      >
+                        alle {formatDate(post.createdAt)}
+                      </p>
+                    </div>
+                    <div className="d-flex gap-1 align-item-center ">
+                      <p style={{ fontSize: "0.7rem", color: "grey" }}>
+                        {post.user.title} |
+                      </p>
+                      <p style={{ fontSize: "0.7rem", color: "grey" }}>
+                        {" "}
+                        {post.user.area}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <p className="p-2 px-4">{post.text}</p>
+              </div>
+            ))}
+        </>
+      ) : (
+        "Caricamento"
+      )}
+    </div>
+  );
+}
+
+export default AllUserPosts;
