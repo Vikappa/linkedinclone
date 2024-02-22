a9170aa790451663e781a95d3d04471c60dd9b7c
Fidan Hykolli <fidan-323@live.com>
2024-02-22 09:29:08 +0100

    sidebar-completed?

--- src/components/StaticSideBar.jsx
+++ src/components/StaticSideBar.jsx
@@ -12,19 +12,19 @@ const Card = () => {
         <div className="card">
             <h2>Titolo della Card</h2>
             <ul>
-                <li>Elemento 1</li>
-                <li>Elemento 2</li>
-                <li>Elemento 3</li>
-                <li>Elemento 4</li>
-                <li>Elemento 5</li>
+                <li>Come fare un bilancio di competenze</li>
+                <li> Il lato positivo dell invidia</li>
+                <li>Ritorno al negozio (del futuro)</li>
+                <li>Verso una pubblica amministrazione ...</li>
+                <li>Il conto del cambiamento climatico</li>
             </ul>
             {showMore && (
                 <ul>
-                    <li>Elemento 6</li>
-                    <li>Elemento 7</li>
-                    <li>Elemento 8</li>
-                    <li>Elemento 9</li>
-                    <li>Elemento 10</li>
+                    <li>Allarme smog in pianura padana</li>
+                    <li>Quando l impostore ci fa crescere</li>
+                    <li>Il richiamo della montagna</li>
+                    <li>Sviluppo immobiliare a Roma Nord</li>
+                    <li>Nuove regole Ue per le batterie</li>
                 </ul>
             )}
             <button onClick={handleShowMore}>{showMore ? 'Nascondi' : 'Vedi altro'}</button>
