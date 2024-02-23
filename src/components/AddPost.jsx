2b81dcfd313cab8ac1e5e69aef40e556f54c3774
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-23 12:22:29 +0100

    fix Addpost

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,145 +1,117 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { FETCH_ALL_POSTS } from "../Redux/Actions/ADD_EXPERIENCE";
-import { Button, Form, FormControl } from "react-bootstrap";
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Form, FormControl } from 'react-bootstrap';
+import { FETCH_ALL_POSTS } from '../Redux/Actions/ADD_EXPERIENCE';
 
-const AddPost = function () {
+const AddPost = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.currentUser.currentUser);
-  const [postText, setPostText] = useState("");
+  const [postText, setPostText] = useState('');
   const [imageFile, setImageFile] = useState(null);
   const [fileInputKey, setFileInputKey] = useState(Date.now());
-  function handleChangeTextInput(e) {
+
+  const handleInputChange = (e) => {
     setPostText(e.target.value);
-  }
+  };
 
-  function handleImageChange(e) {
-    const file = e.target.files[0];
-    setImageFile(file);
-  }
-  async function handleSubmit(e) {
+  const handleFileChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // const postData = {
-    //   text: postText,
-    //   post: imageFile,
-    // };
-
-    const myURl = `https://striveschool-api.herokuapp.com/api/posts/`;
-
-    const bearerToken = sessionStorage.getItem("accessToken");
+    const apiUrl = 'https://striveschool-api.herokuapp.com/api/posts/';
+    const bearerToken = `Bearer ${sessionStorage.getItem('accessToken')}`;
+  
     try {
-      const formData = {
-        text: postText,
-      };
-      const response = await fetch(myURl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + bearerToken,
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (!response.ok) {
-        throw new Error("Errore nella fetch post");
+      // Invio del testo del post e recupero dell'ID del post creato
+      const postId = await postTextContent(apiUrl, postText, bearerToken);
+      if (imageFile) {
+        // Se c'è un file immagine, lo inviamo usando l'ID del post appena creato
+        await postImage(`${apiUrl}${postId}`, imageFile, bearerToken);
       }
-
-      const data = await response.json();
-      console.log("Commento inviato con successo:", data._id);
-      const myUrlImage = `https://striveschool-api.herokuapp.com/api/posts/${data._id}`;
-
-      const formDataImage = new FormData();
-      formDataImage.append("post", imageFile);
-
-      const responseImage = await fetch(myUrlImage, {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + bearerToken,
-        },
-        body: formDataImage,
-      });
-      if (!responseImage.ok) {
-        throw new Error("Errore nella richiesta POST per l'immagine");
-      }
-      await fetchAllPosts();
-      setFileInputKey(Date.now());
-      setImageFile(null);
-      setPostText("");
+      resetForm();
+      fetchAllPosts();
     } catch (error) {
-      console.error("Errore invio del commento:", error);
+      console.error('Errore durante l\'invio del post:', error);
     }
-  }
+  };
+  
+  const postImage = async (url, file, token) => {
+    const formData = new FormData();
+    formData.append('post', file);
+    // Assicurarsi che l'header 'Content-Type' non sia impostato quando si invia FormData
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    });
+    if (!response.ok) throw new Error('Errore nella richiesta POST per l\'immagine');
+  };
+  
 
-  async function fetchAllPosts() {
+  const postTextContent = async (url, text, token) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify({ text }),
+    });
+    if (!response.ok) throw new Error('Errore nella fetch post');
+    const { _id } = await response.json();
+    console.log('Post inviato con successo:', _id);
+    return _id;
+  };
+
+
+  const fetchAllPosts = async () => {
     try {
-      const res = await fetch(
-        `https://striveschool-api.herokuapp.com/api/posts`,
-        {
-          headers: {
-            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-          },
-        }
-      );
-      if (!res.ok) {
-        throw new Error("Errore nel fetch dei post");
-      }
-      const posts = await res.json()
-      dispatch({
-        type: FETCH_ALL_POSTS,
-        payload: posts,
+      const response = await fetch('https://striveschool-api.herokuapp.com/api/posts', {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}` },
       });
+      if (!response.ok) throw new Error('Errore nel fetch dei post');
+      const posts = await response.json();
+      dispatch({ type: FETCH_ALL_POSTS, payload: posts });
     } catch (error) {
-      console.log(error);
+      console.error('Errore nel fetch dei post:', error);
     }
-  }
+  };
 
-  return (
-    <>
-      {currentUser ? (
-        <div
-          className="d-flex bg-white rounded rounded-2 m-1 align-items-center p-3 pb-5 gap-2"
-          style={{ border: "1px solid lightgrey" }}
-        >
-          <img
-            src={currentUser.image}
-            alt="User"
-            height={"60px"}
-            width={"60px"}
-            className="border rounded"
-          />
-          <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
-            <FormControl
-              style={{ width: "100%" }}
-              className="rounded-pill p-2"
-              type="text"
-              placeholder="Avvia una nuova conversazione"
-              value={postText}
-              onChange={handleChangeTextInput}
-            />
-            <FormControl
-              key={fileInputKey}
-              className="mt-2"
-              type="file"
-              onChange={handleImageChange}
-              style={{ width: "50%" }}
-            />
-            <Button
-              type="submit"
-              className="mt-2 text-center"
-              variant="secondary"
-              style={{ width: "15%", height: "10%" }}
-              disabled={postText.length===0}
-            >
-              Invia
-            </Button>
-          </Form>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+  const resetForm = () => {
+    setFileInputKey(Date.now());
+    setImageFile(null);
+    setPostText('');
+  };
+
+  return currentUser ? (
+    <div className="d-flex bg-white rounded rounded-2 m-1 align-items-center p-3 pb-5 gap-2" style={{ border: '1px solid lightgrey' }}>
+      <img src={currentUser.image} alt="User" height="60px" width="60px" className="border rounded" />
+      <Form style={{ width: '100%' }} onSubmit={handleSubmit}>
+        <FormControl
+          className="rounded-pill p-2"
+          type="text"
+          placeholder="Avvia una nuova conversazione"
+          value={postText}
+          onChange={handleInputChange}
+          style={{ width: '100%' }}
+        />
+        <FormControl
+          key={fileInputKey}
+          className="mt-2"
+          type="file"
+          onChange={handleFileChange}
+          style={{ width: '50%' }}
+        />
+        <Button type="submit" className="mt-2 text-center" variant="secondary" disabled={!postText} style={{ width: '15%', height: '10%' }}>
+          Invia
+        </Button>
+      </Form>
+    </div>
+  ) : null;
 };
 
 export default AddPost;
