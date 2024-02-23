359f3314ed77b1a618a195936586a216b73abf1e
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-23 01:15:35 +0100

    Update routing experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ import { Button } from "react-bootstrap";
 import DeleteExperience from "./DeleteExperience";
 import { FaPen } from "react-icons/fa";
 import PutExperience from "./PutExperience";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 function formatISODate(isoString) {
   const date = new Date(isoString);
@@ -23,10 +23,39 @@ const Experience = () => {
   const [currentUser, setCurrentUser] = useState(currentUserStore)
   const [path, setPath] = useState(window.location.pathname)
   const location = useLocation()
+  const { userId } = useParams()
+  const [focusedUserExperiences, setFocusedUserExperiences] = useState([])
+
+  const fetchExperiencesFocusedUser = async () => {
+    try {
+      const fetchedExperiences = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+
+      if (!fetchedExperiences.ok) {
+        throw new Error("Errore nella fetch delle esperienze")
+      }
+
+      const experiencesFocus = await fetchedExperiences.json()
+      setFocusedUserExperiences(experiencesFocus)
+    } catch (error) {
+      console.log("Non sono riuscito a gettare le esperienze di focused user")
+    }
+  };
 
   useEffect(() => {
-    console.log(path)
-  }, [location.pathname])
+
+    fetchExperiencesFocusedUser()
+
+  }, [userId])
+  
 
   const dispatch = useDispatch();
 
@@ -35,7 +64,6 @@ const Experience = () => {
   }, [currentUserStore]);
 
   const storeEsperienze = useSelector((state) => state.experience.experiences);
-  console.log("store esperienza id", storeEsperienze._id);
   const [visibilitàModaleAddEsperienza, setVisibilitàModaleAddEsperienza] =
     useState(false);
 
@@ -96,8 +124,7 @@ const Experience = () => {
   const [visibilitàModaleEditEsperienza, setVisibilitàModaleEditEsperienza] =
     useState(false)
   const handleEditClick = (experience) => {
-    // setEditExperience(experience)
-    setExperienceToEdit(experience);
+    setExperienceToEdit(experience)
     console.log("sperienza id", experience._id);
     setVisibilitàModaleEditEsperienza(true);
   };
@@ -110,86 +137,153 @@ const Experience = () => {
       >
         <div className="d-flex align-items-center justify-content-between">
           <h5 className="p-1">Esperienza</h5>
-          <i
+          {
+            userId ? "" : <i
             className="bi bi-plus-circle"
             style={{ fontSize: "2rem" }}
             onClick={handleAddEsperienza}
           ></i>
+          }
         </div>
-        {storeEsperienze.map((esperienza, index) => (
-          <div key={index} className="d-flex align-items-center border-bottom ">
-            <div className="d-flex justify-content-start align-items-center jobLine p-2">
-              {esperienza.image ? (
-                <img
-                  src={esperienza.image}
-                  alt="image"
-                  className="border rounded-circle me-2"
-                  style={{ width: "40px", height: "40px" }}
-                />
-              ) : (
-                <img
-                  src="https://s3-eu-west-1.amazonaws.com/tpd/logos/62a6277627ee655c1226b624/0x0.png"
-                  alt="image"
-                  className="border rounded-circle me-2"
-                  style={{ width: "40px", height: "40px" }}
+        {
+          focusedUserExperiences.length>0 ? 
+          
+          focusedUserExperiences.map((esperienza, index) => (
+            <div key={index} className="d-flex align-items-center border-bottom ">
+              <div className="d-flex justify-content-start align-items-center jobLine p-2">
+                {esperienza.image ? (
+                  <img
+                    src={esperienza.image}
+                    alt="image"
+                    className="border rounded-circle me-2"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                ) : (
+                  <img
+                    src="https://s3-eu-west-1.amazonaws.com/tpd/logos/62a6277627ee655c1226b624/0x0.png"
+                    alt="image"
+                    className="border rounded-circle me-2"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                )}
+                <div className="d-flex flex-column align-items-start">
+                  <p className="m-0">{esperienza.role}</p>
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.75rem", color: "grey" }}
+                  >
+                    {esperienza.company}
+                  </p>
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.6rem", color: "grey" }}
+                  >
+                    {esperienza.description} - {esperienza.area}
+                  </p>
+                  {esperienza.endDate ? (
+                    <p
+                      className="m-0"
+                      style={{ fontSize: "0.6rem", color: "grey" }}
+                    >
+                      {formatISODate(esperienza.startDate)} -{" "}
+                      {formatISODate(esperienza.endDate)}
+                    </p>
+                  ) : (
+                    <p
+                      className="m-0"
+                      style={{ fontSize: "0.6rem", color: "grey" }}
+                    >
+                      {formatISODate(esperienza.startDate)} - attualmente assunto
+                    </p>
+                  )}
+                </div>
+              </div>
+              {showDeleteModal && deleteExperienceId === esperienza._id && (
+                <DeleteExperience
+                  experienceId={deleteExperienceId}
+                  onClose={handleDeleteModalClose}
                 />
               )}
-              <div className="d-flex flex-column align-items-start">
-                <p className="m-0">{esperienza.role}</p>
-                <p
-                  className="m-0"
-                  style={{ fontSize: "0.75rem", color: "grey" }}
-                >
-                  {esperienza.company}
-                </p>
-                <p
-                  className="m-0"
-                  style={{ fontSize: "0.6rem", color: "grey" }}
-                >
-                  {esperienza.description} - {esperienza.area}
-                </p>
-                {esperienza.endDate ? (
-                  <p
-                    className="m-0"
-                    style={{ fontSize: "0.6rem", color: "grey" }}
-                  >
-                    {formatISODate(esperienza.startDate)} -{" "}
-                    {formatISODate(esperienza.endDate)}
-                  </p>
-                ) : (
-                  <p
-                    className="m-0"
-                    style={{ fontSize: "0.6rem", color: "grey" }}
-                  >
-                    {formatISODate(esperienza.startDate)} - attualmente assunto
-                  </p>
-                )}
-              </div>
             </div>
-            <Button
-              style={{ width: "40px", height: "40px" }}
-              variant="light"
-              onClick={() => handleEditClick(esperienza)}
-              className="d-flex align-items-center me-1"
-            >
-              <FaPen />
-            </Button>
-            <Button
-              style={{ width: "40px", height: "40px" }}
-              variant="danger"
-              onClick={() => handleDeleteClick(esperienza._id)}
-              className="d-flex align-items-center"
-            >
-              <FaTrashAlt />
-            </Button>
-            {showDeleteModal && deleteExperienceId === esperienza._id && (
-              <DeleteExperience
-                experienceId={deleteExperienceId}
-                onClose={handleDeleteModalClose}
-              />
-            )}
-          </div>
-        ))
+          ))
+          
+          : 
+          
+          storeEsperienze.map((esperienza, index) => (
+            <div key={index} className="d-flex align-items-center border-bottom ">
+              <div className="d-flex justify-content-start align-items-center jobLine p-2">
+                {esperienza.image ? (
+                  <img
+                    src={esperienza.image}
+                    alt="image"
+                    className="border rounded-circle me-2"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                ) : (
+                  <img
+                    src="https://s3-eu-west-1.amazonaws.com/tpd/logos/62a6277627ee655c1226b624/0x0.png"
+                    alt="image"
+                    className="border rounded-circle me-2"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                )}
+                <div className="d-flex flex-column align-items-start">
+                  <p className="m-0">{esperienza.role}</p>
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.75rem", color: "grey" }}
+                  >
+                    {esperienza.company}
+                  </p>
+                  <p
+                    className="m-0"
+                    style={{ fontSize: "0.6rem", color: "grey" }}
+                  >
+                    {esperienza.description} - {esperienza.area}
+                  </p>
+                  {esperienza.endDate ? (
+                    <p
+                      className="m-0"
+                      style={{ fontSize: "0.6rem", color: "grey" }}
+                    >
+                      {formatISODate(esperienza.startDate)} -{" "}
+                      {formatISODate(esperienza.endDate)}
+                    </p>
+                  ) : (
+                    <p
+                      className="m-0"
+                      style={{ fontSize: "0.6rem", color: "grey" }}
+                    >
+                      {formatISODate(esperienza.startDate)} - attualmente assunto
+                    </p>
+                  )}
+                </div>
+              </div>
+              <Button
+                style={{ width: "40px", height: "40px" }}
+                variant="light"
+                onClick={() => handleEditClick(esperienza)}
+                className="d-flex align-items-center me-1"
+              >
+                <FaPen />
+              </Button>
+              <Button
+                style={{ width: "40px", height: "40px" }}
+                variant="danger"
+                onClick={() => handleDeleteClick(esperienza._id)}
+                className="d-flex align-items-center"
+              >
+                <FaTrashAlt />
+              </Button>
+              {showDeleteModal && deleteExperienceId === esperienza._id && (
+                <DeleteExperience
+                  experienceId={deleteExperienceId}
+                  onClose={handleDeleteModalClose}
+                />
+              )}
+            </div>
+          ))
+          
         }
       </div>
       <ModaleAggiungiEsperienza
